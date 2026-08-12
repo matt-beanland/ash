945fcec339ff282dd4216024ff7257c3ea51fbe8
Matt Beanland <beanland@live.com.au>
2026-08-13 01:05:53 +0930

    feat: add precision constraint to Ash.Type.NaiveDatetime

--- lib/ash/type/naive_datetime.ex
+++ lib/ash/type/naive_datetime.ex
@@ -3,15 +3,37 @@
 # SPDX-License-Identifier: MIT
 
 defmodule Ash.Type.NaiveDatetime do
+  @constraints [
+    precision: [
+      type: {:one_of, [:microsecond, :second]},
+      default: :second
+    ]
+  ]
+
   @moduledoc """
-  Represents a Naive datetime
+  Represents a Naive datetime, with configurable precision.
 
   A builtin type that can be referenced via `:naive_datetime`
+
+  ### Constraints
+
+  #{Spark.Options.docs(@constraints)}
   """
   use Ash.Type
 
   @impl true
-  def storage_type(_), do: :naive_datetime
+  def constraints, do: @constraints
+
+  @impl true
+  def init(constraints) do
+    {precision, constraints} = Keyword.pop(constraints, :precision)
+    precision = precision || :second
+    {:ok, [{:precision, precision} | constraints]}
+  end
+
+  @impl true
+  def storage_type([{:precision, :microsecond} | _]), do: :naive_datetime_usec
+  def storage_type(_constraints), do: :naive_datetime
 
   @impl true
   def generator(_constraints) do
@@ -23,11 +45,30 @@ defmodule Ash.Type.NaiveDatetime do
   @impl true
   def cast_input(nil, _), do: {:ok, nil}
 
-  def cast_input(value, _) do
-    Ecto.Type.cast(:naive_datetime, value)
+  def cast_input(
+        %NaiveDateTime{microsecond: {_, _} = microseconds} = datetime,
+        [{:precision, :second} | _] = constraints
+      )
+      when microseconds != {0, 0} do
+    cast_input(%{datetime | microsecond: {0, 0}}, constraints)
+  end
+
+  def cast_input(
+        %NaiveDateTime{microsecond: {0, 0}} = datetime,
+        [{:precision, :microsecond} | _] = constraints
+      ) do
+    cast_input(%{datetime | microsecond: {0, 6}}, constraints)
+  end
+
+  def cast_input(value, constraints) do
+    Ecto.Type.cast(storage_type(constraints), value)
   end
 
   @impl true
+  def matches_type?(%NaiveDateTime{microsecond: {usec, _}}, constraints) when usec != 0 do
+    Keyword.get(constraints, :precision, :second) == :microsecond
+  end
+
   def matches_type?(%NaiveDateTime{}, _), do: true
   def matches_type?(_, _), do: false
 
@@ -43,16 +84,16 @@ defmodule Ash.Type.NaiveDatetime do
     cast_input(value, constraints)
   end
 
-  def cast_stored(value, _) do
-    Ecto.Type.load(:naive_datetime, value)
+  def cast_stored(value, constraints) do
+    Ecto.Type.load(storage_type(constraints), value)
   end
 
   @impl true
 
   def dump_to_native(nil, _), do: {:ok, nil}
 
-  def dump_to_native(value, _) do
-    Ecto.Type.dump(:naive_datetime, value)
+  def dump_to_native(value, constraints) do
+    Ecto.Type.dump(storage_type(constraints), value)
   end
 end
 
--- test/type/naive_datetime_test.exs
+++ test/type/naive_datetime_test.exs
@@ -6,6 +6,8 @@ defmodule Ash.Test.Type.NaiveDateTimeTest do
   @moduledoc false
   use ExUnit.Case, async: true
 
+  require Ash.Query
+
   alias Ash.Test.Domain, as: Domain
 
   defmodule Post do
@@ -29,6 +31,11 @@ defmodule Ash.Test.Type.NaiveDateTimeTest do
       end
 
       attribute :naive_datetime_b, :naive_datetime, allow_nil?: false, public?: true
+
+      attribute :naive_datetime_usec, :naive_datetime do
+        constraints precision: :microsecond
+        public?(true)
+      end
     end
   end
 
@@ -43,5 +50,39 @@ defmodule Ash.Test.Type.NaiveDateTimeTest do
 
     assert post.naive_datetime_a == ~N[2022-04-17 08:30:00]
     assert post.naive_datetime_b == ~N[2022-04-17 15:45:30]
+  end
+
+  test "it truncates to the second by default" do
+    post = create_post(%{naive_datetime_a: ~N[2022-04-17 08:30:00.123456]})
+
+    assert post.naive_datetime_a == ~N[2022-04-17 08:30:00]
+  end
+
+  test "it keeps microseconds when the precision says to" do
+    post = create_post(%{naive_datetime_usec: ~N[2022-04-17 08:30:00.123456]})
+
+    assert post.naive_datetime_usec == ~N[2022-04-17 08:30:00.123456]
+  end
+
+  test "it reads microseconds back out of storage" do
+    post = create_post(%{naive_datetime_usec: ~N[2022-04-17 08:30:00.123456]})
+
+    assert [%{naive_datetime_usec: ~N[2022-04-17 08:30:00.123456]}] =
+             Ash.read!(Ash.Query.filter(Post, id == ^post.id))
+  end
+
+  test "it pads a whole second out to microsecond precision" do
+    post = create_post(%{naive_datetime_usec: ~N[2022-04-17 08:30:00]})
+
+    assert post.naive_datetime_usec.microsecond == {0, 6}
+  end
+
+  defp create_post(attrs) do
+    Post
+    |> Ash.Changeset.for_create(
+      :create,
+      Map.put_new(attrs, :naive_datetime_b, ~N[2022-04-17 15:45:30])
+    )
+    |> Ash.create!()
   end
 end
