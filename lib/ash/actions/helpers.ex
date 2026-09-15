a2dd6ccad2c65bfff9222522f24b761a40224e24
Matt Beanland <beanland@live.com.au>
2026-09-16 00:00:41 +0930

    fix: narrow a range-valued as_of on the opts path as the query setter does

--- lib/ash/actions/helpers.ex
+++ lib/ash/actions/helpers.ex
@@ -427,6 +427,10 @@ defmodule Ash.Actions.Helpers do
   defp resolve_query_as_of(_query, :now), do: DateTime.utc_now()
   defp resolve_query_as_of(_query, %DateTime{} = as_of), do: as_of
 
+  # Narrowed here as well as in `Ash.Query.as_of/2`: `as_of:` in opts and `as_of/2` on the
+  # query reach different code and must not answer differently.
+  defp resolve_query_as_of(_query, %Ash.Range{} = as_of), do: as_of
+
   defp resolve_query_as_of(query, nil) do
     if Ash.Resource.Info.temporal?(query.resource), do: DateTime.utc_now()
   end
